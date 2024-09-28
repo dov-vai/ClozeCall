24064675e7cd050c2e67306087014c32801af7f6
dov-vai <160327869+dov-vai@users.noreply.github.com>
2024-09-28 23:56:39 +0300

    fix(cloze-service): no more locking up in the loop
    the variable names were clashing and I didn't check, whoops

--- lib/services/cloze_service.dart
+++ lib/services/cloze_service.dart
@@ -5,18 +5,29 @@ import 'package:file_picker/file_picker.dart';
 class Cloze {
   final String original;
   final String translated;
-  final String removedWord;
-  final List<String> randomWords;
+  final String answer;
+  final List<String> words;
 
-  Cloze({required this.original, required this.translated, required this.removedWord, required this.randomWords});
+  Cloze(
+      {required this.original,
+      required this.translated,
+      required this.answer,
+      required this.words});
 }
 
 class ClozeService {
   late File? _file;
   late List<String> _lines;
   final Random _random = Random();
+  bool _initialized = false;
+
+  bool get initialized => _initialized;
 
   Future<void> initialize() async {
+    if (_initialized) {
+      return;
+    }
+
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['tsv'],
@@ -25,13 +36,14 @@ class ClozeService {
     if (result != null) {
       _file = File(result.files.single.path!);
       _lines = await _file!.readAsLines();
+      _initialized = true;
     } else {
       throw Exception('No file selected.');
     }
   }
 
-  Future<Cloze> getRandomCloze() async {
-    if (_file == null){
+  Cloze getRandomCloze() {
+    if (!_initialized) {
       throw Exception("Cloze Service is not initialized!");
     }
     String line = _lines[_random.nextInt(_lines.length)];
@@ -45,20 +57,20 @@ class ClozeService {
 
     List<String> randomWords = [];
     while (randomWords.length < 3) {
-      String randomLine = _lines[_random.nextInt(_lines.length)];
-      List<String> randomWords = randomLine.split(' ');
-      String randomWord = randomWords[_random.nextInt(randomWords.length)];
+      String randomLine = _lines[_random.nextInt(_lines.length)].split('\t')[0];
+      List<String> words = randomLine.split(' ');
+      String word = words[_random.nextInt(words.length)];
 
-      if (randomLine != line && !randomWords.contains(randomWord)) {
-        randomWords.add(randomWord);
+      if (randomLine != line && !randomWords.contains(word)) {
+        randomWords.add(word);
       }
     }
+    randomWords.add(removedWord);
 
     return Cloze(
         original: sentences[0],
         translated: sentences[1],
-        removedWord: removedWord,
-        randomWords: randomWords
-    );
+        answer: removedWord,
+        words: randomWords);
   }
 }
